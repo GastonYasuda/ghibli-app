1b6a4a5f7c40ec7582b4100f9abcf9b434583261
GastonYasuda <gastonyasuda@gmail.com>
2025-01-02 23:57:00 -0300

    navlink clas

--- src/app/components/NavLinks.tsx
+++ src/app/components/NavLinks.tsx
@@ -7,8 +7,8 @@ import { usePathname } from 'next/navigation';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
     { name: 'Home', href: '/' },
-    { name: 'directors', href: '/directors' },
-    { name: 'favorites', href: '/favorites' },
+    { name: 'Directors', href: '/directors' },
+    { name: 'Favorites', href: '/favorites' },
 ];
 
 export default function NavLinks() {
@@ -16,18 +16,19 @@ export default function NavLinks() {
     const pathname = usePathname()
 
     return (
-        <>
+        <section className='mt-10'>
             {links.map((link) => {
                 return (
                     <Link
                         key={link.name}
                         href={link.href}
-                        className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${pathname === link.href ? 'bg-sky-100 text-blue-600' : ''}`}
+                        className={`flex h-[48px] grow items-center m-1 justify-start rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 text-red
+                            ${pathname === link.href ? 'bg-sky-100 text-blue-600' : ''}`}
                     >
-                        <p className="hidden md:block">{link.name}</p>
+                        <p className="">{link.name}</p>
                     </Link>
                 );
             })}
-        </>
+        </section>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
 
   return (
-    <div className="ghibliTest">
+    <div>
 
       {ghibliAllFilms.map((eachMovie, index) => {
         return (
