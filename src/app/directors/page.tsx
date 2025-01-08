0b63442387b95f03b76598859d6e21427970f044
GastonYasuda <gastonyasuda@gmail.com>
2025-01-07 23:02:33 -0300

    change directorPage Route

--- src/app/directors/page.tsx
+++ src/app/directors/page.tsx
@@ -1,60 +1,14 @@
+
 'use client'
-import { useEffect, useState } from "react";
-import { fetchGhibliByDirector, fetchGhibliByDirectorName } from "../data/data";
-import { GhibliDirectorType } from "@/Types/ghibliType";
-import ShowDirectorsMovies from "../components/ShowDirectorsMovies";
 import { usePathname } from "next/navigation";
-interface DirectorPageProps {
-    directorId?: string;
-}
+import DirectorPage from "./[director]/DirectorPage";
 
-export default function DirectorPage({ directorId }: DirectorPageProps) {
-
-    const [movieByDirector, setMovieByDirector] = useState<GhibliDirectorType>();
-    const [AllmoviesByDirectorArray, setAllMoviesByDirectorArray] = useState<GhibliDirectorType[]>([]);
-
-    const pathName = usePathname().split("/").pop()
+export default function DirectorByNamePage() {
+    const pathName = usePathname()
+    const pathNameReplace = pathName.replace(/%20/g, " ").split("/").pop();
 
 
-    useEffect(() => {
 
-        const getDirectorData = async () => {
-            try {
-                if (directorId) {
-                    const directorsAndMovies = await fetchGhibliByDirectorName(directorId);
-                    setMovieByDirector(directorsAndMovies);
 
-                } else if (!directorId) {
-                    const directorsAndMovies = await fetchGhibliByDirector();
-                    setAllMoviesByDirectorArray(directorsAndMovies);
-                }
-            } catch (error) {
-                console.error("Error fetching director:", error);
-            }
-        }
-        getDirectorData()
-
-    }, [directorId])
-
-    return (
-        <section className="ml-48">
-            {pathName !== 'directors' && movieByDirector !== undefined ?
-                <>
-                    <h1>{movieByDirector.name}</h1>
-                    <ShowDirectorsMovies movies={movieByDirector.movies} />
-                </>
-                :
-                <>
-                    {AllmoviesByDirectorArray?.map((director, i) => {
-                        return (
-                            <div key={i}>
-                                <h1 className="font-extrabold text-4xl text-center mb-8">{director.name}</h1>
-                                <ShowDirectorsMovies movies={director.movies} />
-                            </div>
-                        )
-                    })}
-                </>
-            }
-        </section>
-    )
+    return <DirectorPage directorId={pathNameReplace} />
 }
